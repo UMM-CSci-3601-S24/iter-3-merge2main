65222c7de63e60b71aadf5c68e7608ee376d43a3
Ken <brod0268@morris.umn.edu>
2024-04-19 02:02:30 -0500

    Created photo dialog testing spec

---
+++ client/src/app/endedHunts/endedHuntDetails/photo-dialog/photo-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhotoDialogComponent } from './photo-dialog.component';
+
+describe('PhotoDialogComponent', () => {
+  let component: PhotoDialogComponent;
+  let fixture: ComponentFixture<PhotoDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoDialogComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PhotoDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
