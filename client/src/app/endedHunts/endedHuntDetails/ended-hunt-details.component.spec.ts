c335131b6f1de1e20a59532fbda9e3528f167294
Ken <brod0268@morris.umn.edu>
2024-04-19 02:53:52 -0500

    Added testing for the endedhunt details component
    Gained coverage on new methods, as well as some old methods which were missed on iteration-2

--- client/src/app/endedHunts/endedHuntDetails/ended-hunt-details.component.spec.ts
+++ client/src/app/endedHunts/endedHuntDetails/ended-hunt-details.component.spec.ts
@@ -9,6 +9,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { EndedHuntCardComponent } from '../ended-hunt-card.component';
 import { ActivatedRoute } from '@angular/router';
 import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { EndedHunt } from '../endedHunt';
+import { MatDialog } from '@angular/material/dialog';
+import { PhotoDialogComponent } from './photo-dialog/photo-dialog.component';
 
 describe('EndedHuntDetailsComponent', () => {
   let component: EndedHuntDetailsComponent;
@@ -18,6 +21,8 @@ describe('EndedHuntDetailsComponent', () => {
   const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub({
     id: chrisId,
   });
+  let dialog: MatDialog;
+  let dialogSpy: jasmine.Spy;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -33,17 +38,70 @@ describe('EndedHuntDetailsComponent', () => {
       providers: [
         { provide: HostService, useValue: mockHostService },
         { provide: ActivatedRoute, useValue: activatedRoute },
+        { provide: MatDialog, useValue: { open: jasmine.createSpy('open') } },
       ],
     }).compileComponents();
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EndedHuntDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    dialog = TestBed.inject(MatDialog);
+    dialogSpy = dialog.open as jasmine.Spy;
   });
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get task name', () => {
+    const mockEndedHunt: EndedHunt = {
+      startedHunt: {
+        _id: 'some_id',
+        accessCode: 'some_access_code',
+        completeHunt: {
+          hunt: {
+            _id: 'hunt_id',
+            hostId: 'host_id',
+            name: 'hunt_name',
+            description: 'hunt_description',
+            est: 20,
+            numberOfTasks: 4
+          },
+          tasks: [
+            {
+              _id: 'task1', name: 'Task 1',
+              huntId: '',
+              status: false,
+              photos: []
+            },
+            {
+              _id: 'task2', name: 'Task 2',
+              huntId: '',
+              status: false,
+              photos: []
+            },
+          ],
+        },
+      },
+      finishedTasks: [],
+    };
+    component.endedHunt = mockEndedHunt;
+    expect(component.getTaskName('task1')).toEqual('Task 1');
+    expect(component.getTaskName('task2')).toEqual('Task 2');
+  });
+
+  it('should open dialog', () => {
+    const mockPhoto = 'photo1';
+    const mockPhotos = ['photo1', 'photo2', 'photo3'];
+    component.openDialog(mockPhoto, mockPhotos);
+    expect(dialogSpy).toHaveBeenCalledWith(PhotoDialogComponent, {
+      data: {
+        currentPhoto: mockPhoto,
+        photos: mockPhotos,
+      },
+    });
   });
 });
