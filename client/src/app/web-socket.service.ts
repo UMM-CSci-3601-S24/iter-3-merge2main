05491dc71e859f54f532fefd93b18c978171bc11
Jaden Rainey <raine123@morris.umn.edu>
2024-04-19 12:01:50 -0500

    web-socket.service added

---
+++ client/src/app/web-socket.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { WebSocketSubject } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+  private socket$: WebSocketSubject<unknown>;
+
+  constructor() {
+    this.socket$ = new WebSocketSubject('ws://localhost:4567/ws');
+  }
+
+  sendMessage(data: unknown) {
+    this.socket$.next(data);
+  }
+
+  onMessage() {
+    return this.socket$.asObservable();
+  }
+}
