0a75ff14e1d7f7df8fc94e6d3931c742b7cfe3b6
Jaden Rainey <raine123@morris.umn.edu>
2024-04-25 13:57:10 -0500

    .

--- client/src/app/web-socket.service.ts
+++ client/src/app/web-socket.service.ts
@@ -8,7 +8,7 @@ export class WebSocketService {
   private socket$: WebSocketSubject<unknown>;
 
   constructor() {
-    this.socket$ = new WebSocketSubject('ws://' + window.location.hostname + '4567/ws/host');
+    this.socket$ = new WebSocketSubject('ws://' + window.location.hostname + ':4567/ws/host');
   }
   // use this when testing 'ws://' + window.location.hostname + ':4567/ws/host'
 
